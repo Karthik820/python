c4bf5866fa85fb7134d976ab882e99ad317140ca
Karthik820 <149553266+Karthik820@users.noreply.github.com>
2024-04-22 12:22:49 +0530

    Add files via upload

---
+++ split.py
@@ -0,0 +1,9 @@
+'''x,y,z=input('enter 3 values').split('?')
+print(x)
+print(y)
+print(z)'''
+
+print('25','12','2024',sep='-')
+print('tested Chandu by ',end='')
+print('Chinnu',end='')
+print(' and Srinu')
