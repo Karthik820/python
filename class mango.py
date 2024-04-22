13abd33dd21681f5bd60457c68f67713a5350dc2
Karthik820 <149553266+Karthik820@users.noreply.github.com>
2024-04-22 11:49:21 +0530

    Add files via upload

---
+++ class mango.py
@@ -0,0 +1,17 @@
+class Mango:
+    def init(self):
+        print("this is what?")
+    def sanjay(self):
+        print("this is without para")
+    def shaid(self,a,b):
+        print(a+b,"this is with para")
+    def magicalprime(self,a):
+        print("check it magical prime or not")
+    def neon(self,a):
+        print("check neon or not")
+man=Mango()
+man.sanjay()
+man.shaid(10,20.5)
+man.magicalprime(101)
+man.neon(7)
+
