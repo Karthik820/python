e361f2a02ef30d335c709e0876a542238aa4a0fa
Karthik820 <149553266+Karthik820@users.noreply.github.com>
2024-04-22 21:58:54 +0530

    Add files via upload

---
+++ iflogin.py
@@ -0,0 +1,8 @@
+email="karthik@gmail.com"
+pwd=56789
+uemail=str(input("Enter the email"))
+upwd=int(input("Enter the pwd"))
+if(email == uemail and pwd==upwd):
+    print("login success")
+else:
+    print("login failed")
