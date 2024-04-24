bd5af2261ceded06477448c32c5541e8abfeeab6
Karthik820 <149553266+Karthik820@users.noreply.github.com>
2024-04-24 09:34:30 +0530

    Update dict 1.py

--- dict 1.py
+++ dict 1.py
@@ -1,5 +1,5 @@
 def priya():
-    result"this is my balance"
+    print("this is my balance")
 test_dict={"fname":priya,"age":50,"address":"salem"}
 print("the original dictionary is:"+str(test_dict))
 res=test_dict['fname']()
